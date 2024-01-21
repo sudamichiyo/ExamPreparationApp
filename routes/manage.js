266e4087e7f385a9344c07fab259e170ee7a5c3b
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2024-01-21 23:20:38 +0900

    [add]問題文と選択肢と答えをデータベースに登録する処理を追加

--- routes/manage.js
+++ routes/manage.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const ps = require('@prisma/client');
+const prisma = new ps.PrismaClient();
+
 /* GET Manage page. */
 router.get('/', (req, res, next) => {
     var data = {
@@ -16,5 +19,38 @@ router.get('/register', (req, res, next) => {
     };
     res.render('register', data);
 });
+
+// http://localhost:3000/manage/registerでPOST送信された時
+// つまりはRegisterページで登録ボタンが押された時
+router.post('/register', async (req, res, next) => {
+    // console.log(req.body.choose_text);
+    // console.log(req.body.choose);
+    // 問題文の登録
+    const question = await prisma.Question.create({
+        data:{
+            question: req.body.question
+        }
+    });
+
+    // 選択肢の登録
+    for (const [index, text] of Object.entries(req.body.choose_text)) {
+        console.log(`${index}: ${text}`)
+        console.log(+index === +req.body.choose);
+        console.log(+req.body.choose);
+
+        const choose = await prisma.Choose.create({
+            data:{
+               question_id: question.id,
+               choose: text,
+               correct: +index === +req.body.choose 
+            }
+        });
+        console.log(choose);
+      }
+    res.redirect('/manage');
+   
+    
+    
+})
 
 module.exports = router;
