60932551e060a384aa146299d33f88a064826e80
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2024-01-24 02:30:40 +0900

    [add] 問題をランダムに取得する処理を追加

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const ps = require('@prisma/client');
+const prisma = new ps.PrismaClient();
+
 /* GET Quiz page. */
-router.get('/', (req, res, next)=> {
+// http://localhost:3000/quizにアクセスされた時
+// 問題の表示
+router.get('/', async(req, res, next)=> {
   res.render('quiz', { title: 'Quiz' });
+  const questions = await prisma.Question.findMany()
+
+  console.log( questions[ Math.floor( Math.random() * questions.length ) ] );
 });
 
 module.exports = router;
