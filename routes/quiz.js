90e23b91170baf3022597df82155c84ceaf93578
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2024-01-25 11:16:35 +0900

    [add] 問題をランダムで表示する処理を追加

--- routes/quiz.js
+++ routes/quiz.js
@@ -8,10 +8,21 @@ const prisma = new ps.PrismaClient();
 // http://localhost:3000/quizにアクセスされた時
 // 問題の表示
 router.get('/', async(req, res, next)=> {
-  res.render('quiz', { title: 'Quiz' });
   const questions = await prisma.Question.findMany()
-
-  console.log( questions[ Math.floor( Math.random() * questions.length ) ] );
+// ランダムな問題を取得
+  const quiz = questions[ Math.floor( Math.random() * questions.length )];
+// 取得したランダムな問題に対する選択肢を全て取得
+  const chooses = await prisma.Choose.findMany({
+    where: {question_id: quiz.id}
+  })
+  var data = {
+    title: 'Quiz',
+    question: quiz,
+    chooses: chooses
+  }
+  res.render('quiz', data);
+  console.log( quiz );
+  console.log(chooses);
 });
 
 module.exports = router;
