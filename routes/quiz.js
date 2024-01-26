e76d56f7deee4da60dcf4df85877baddb85adcfb
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2024-01-27 02:32:01 +0900

    [add] 間違った問題のみを解く処理を追加

--- routes/quiz.js
+++ routes/quiz.js
@@ -9,6 +9,10 @@ const prisma = new ps.PrismaClient();
 // 問題の表示
 router.get('/', async(req, res, next)=> {
   const questions = await prisma.Question.findMany()
+  if (questions.length === 0) {
+    res.render('nothing',{ title: '問題がありません' })
+    return
+  }
 // ランダムな問題を取得
   const quiz = questions[ Math.floor( Math.random() * questions.length )];
 // 取得したランダムな問題に対する選択肢を全て取得
@@ -19,12 +23,39 @@ router.get('/', async(req, res, next)=> {
   var data = {
     title: 'Quiz',
     question: quiz,
-    chooses: chooses
+    chooses: chooses,
+    mode: 'all'
   }
   res.render('quiz', data);
-  console.log( quiz );
-  console.log(chooses);
+//   console.log( quiz );
+//   console.log(chooses);
 });
+
+router.get('/wrong', async(req, res, next)=> {
+    const questions = await prisma.Question.findMany({
+        where: { isWrong: true }
+    })
+    if (questions.length === 0) {
+        res.render('nothing',{ title: '問題がありません' })
+        return
+    }
+  // ランダムな問題を取得
+    const quiz = questions[ Math.floor( Math.random() * questions.length )];
+  // 取得したランダムな問題に対する選択肢を全て取得
+    const chooses = await prisma.Choose.findMany({
+      where: {question_id: quiz.id}
+    })
+  // 取得した問題と選択肢を渡してレンダリングする
+    var data = {
+      title: 'WrongQuiz',
+      question: quiz,
+      chooses: chooses,
+      mode: 'wrong'
+    }
+    res.render('quiz', data);
+  //   console.log( quiz );
+  //   console.log(chooses);
+  });
 
 router.get('/correct', async(req, res, next) => {
     var question_id = +req.query.question_id;
@@ -42,8 +73,14 @@ router.get('/correct', async(req, res, next) => {
             isWrong: correctIndex !== choose
         }
     });
+    var data = {
+        title: 'Correct', 
+        isCorrect: correctIndex === choose, 
+        correctNumber: correctIndex, 
+        mode: req.query.mode
+    }
 
-    res.render('correct',{ title: 'Correct', isCorrect: correctIndex === choose, correctNumber: correctIndex});
+    res.render('correct', data);
 });
 
 module.exports = router;
